78329b5dc6eafdc9b4a7af5a4722724ce62fd4d7
carlhutant <carlhutant@gmail.com>
2021-10-02 23:44:28 +0800

    2021.10.02 23:44 residual batch

--- FineTuneResNet101.py
+++ FineTuneResNet101.py
@@ -20,64 +20,68 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import SGD
 
-# Import data
-# change the dataset here###
-dataset = 'AWA2'
 # datatype: img, tfrecord
+# data_advance: color_diff_121, none
+# preprocess: caffe
+# color_mode: BGR, RGB, none
+dataset = 'AWA2'
 datatype = 'img'
-# data data_advance: color_diff_121, none
 data_advance = 'none'
-# preprocess
 preprocess = 'caffe'
-# crop
-crop_type = 'random'
-##############################
+color_mode = "BGR"
 
-batch_size = 16
+train_shuffle = False
+train_shuffle_every_epoch = False
+val_shuffle = False
+val_shuffle_every_epoch = False
+random_seed = 486
+
+batch_size = 256
+train_final_batch_opt = 'complete'
+val_final_batch_opt = 'complete'
+train_resize_short_edge_max = 256
+train_resize_short_edge_min = 256
+train_IMG_SHAPE = 224
+
+train_horizontal_flip = False
+train_crop_type = 'center'
+train_crop_w = train_IMG_SHAPE
+train_crop_h = train_IMG_SHAPE
+
+multi_GPU = False
+GPU_memory_growth = False
+
+
 dataset_dir = configure.dataset_dir
 model_dir = configure.model_dir
 train_dir = '{}/{}/{}/{}/train/'.format(dataset_dir, dataset, datatype, data_advance)
 val_dir = '{}/{}/{}/{}/val/'.format(dataset_dir, dataset, datatype, data_advance)
-ckpt_dir = '{}/{}/{}/{}/{}_crop/'.format(model_dir, dataset, datatype, data_advance, crop_type)
-model_save_path = '{}/{}/{}/{}/{}_crop/final/'.format(model_dir, dataset, datatype, data_advance, crop_type)
-IMG_SHAPE = 224
-dataset_shrink_ratio = 1
-multi_GPU = False
-GPU_memory_growth = False
+ckpt_dir = '{}/{}/{}/{}/{}_crop/'.format(model_dir, dataset, datatype, data_advance, train_crop_type)
+model_save_path = '{}/{}/{}/{}/{}_crop/final/'.format(model_dir, dataset, datatype, data_advance, train_crop_type)
+
 
 if GPU_memory_growth:
     gpus = tf.config.experimental.list_physical_devices('GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
 
+
 if dataset == 'AWA2':
-    class_attr_shape = (85,)
-    class_attr_dim = 85
-    class_num = 50
-    seen_class_num = 40
-    unseen_class_num = 10
+    class_num = 40
     file_type = '.jpg'
     train_cardinality = 24264
     val_cardinality = 6070
 elif dataset == 'imagenet':
     class_num = 1000
-    seen_class_num = 1000
     file_type = '.JPEG'
     train_cardinality = 1281167
     val_cardinality = 50000
 else:
-    class_num = -1
-    seen_class_num = -1
-    file_type = '...'
-    train_cardinality = -1
-    val_cardinality = -1
-
-train_cardinality = train_cardinality // dataset_shrink_ratio
-val_cardinality = val_cardinality // dataset_shrink_ratio
+    raise RuntimeError
 
 
 # 考慮 shuffle every epoch
-def img_generator(target_directory, color_mode, shuffle=False):
+def img_generator(target_directory, shuffle, shuffle_every_epoch):
     walk_generator = os.walk(target_directory)
     root, directory, _ = next(walk_generator)
     instance_list = []
@@ -90,37 +94,60 @@ def img_generator(target_directory, color_mode, shuffle=False):
                 instance_list.append({'path': os.path.join(flies_root, file), 'label': class_count})
         class_count = class_count + 1
     file_num = len(instance_list)
-    # print("Found {} images belonging to {} classes.".format(file_num, class_count))
-    if shuffle:
-        new_instance_list = []
-        while len(instance_list):
-            rand_num = random.randint(0, len(instance_list) - 1)
-            new_instance_list.append(instance_list[rand_num])
-            del instance_list[rand_num]
-        instance_list = new_instance_list
-    instance_list_len = len(instance_list)
+    if shuffle or shuffle_every_epoch:
+        random.shuffle(instance_list)
+    yield file_num, class_count
     while True:
-        for i in range(0, instance_list_len, dataset_shrink_ratio):
+        for i in range(file_num):
             instance = instance_list[i]
             img = cv2.imread(instance['path'])
-
             if color_mode == "RGB":
-                img = img[..., ::-1]
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
             img = np.array(img, dtype=np.float32)
-            label = np.zeros(seen_class_num, dtype=np.float32)
+            label = np.zeros(class_num, dtype=np.float32)
             label[instance['label']] = 1
             yield img, label
+        if shuffle_every_epoch:
+            random.shuffle(instance_list)
 
 
+###############################################
+# target_directory: 目錄
+# batch_size
+# crop_type: random, center, none
+# crop_h: height
+# crop_w: width
+# resize_short_edge_max
+# resize_short_edge_min
+# horizontal_flip: True, False
+# color_mode='BGR', 'RGB'
+# shuffle: True, False
+# seed: random seed
+###############################################
 # 考慮加入 last batch 捨去功能, rotate
-def crop_generator(target_directory, batch_size=1, crop_type=None, crop_w=256, crop_h=256, resize_short_edge_max=256,
-                   resize_short_edge_min=256, horizontal_flip=False, color_mode="BGR", shuffle=True, seed=0):
-    img_gen = img_generator(target_directory, shuffle=shuffle, color_mode=color_mode)
-    random.seed(seed)
+def crop_generator(target_directory, final_batch_opt, crop_type, crop_h, crop_w, resize_short_edge_max,
+                   resize_short_edge_min, horizontal_flip, shuffle, shuffle_every_epoch):
+    img_gen = img_generator(target_directory, shuffle, shuffle_every_epoch)
+    file_num, dir_num = next(img_gen)
+    print("Found {} images belonging to {} classes.".format(file_num, dir_num))
+    if file_num != train_cardinality and file_num != val_cardinality:
+        raise RuntimeError
+    if dir_num != class_num:
+        raise RuntimeError
+    yield
+    random.seed(random_seed)
+    file_remain_num = file_num
     while True:
         batch_feature = np.empty([0, crop_h, crop_w, 3], dtype=np.float32)
-        batch_label = np.empty([0, seen_class_num], dtype=np.float32)
-        for i in range(batch_size):
+        batch_label = np.empty([0, class_num], dtype=np.float32)
+        if final_batch_opt == 'complete':
+            batch_data_num = min(batch_size, file_remain_num)
+        elif final_batch_opt == 'full':
+            batch_data_num = batch_size
+        else:
+            print('final_batch_opt error.')
+            raise RuntimeError
+        for i in range(batch_data_num):
             img, label = next(img_gen)
             if horizontal_flip:
                 if random.randint(0, 1):
@@ -133,17 +160,17 @@ def crop_generator(target_directory, batch_size=1, crop_type=None, crop_w=256, c
                 new_width = random.randint(resize_short_edge_min, resize_short_edge_max)
                 new_height = round(height * new_width / width)
             img = cv2.resize(img, (new_width, new_height))
-            if crop_type == None:
+            if crop_type == 'center':  # center crop 尚未完成
                 crop = cv2.resize(img, (crop_w, crop_h))
-            elif crop_type == "random":  # 先不處理 crop 過大的情況
+            elif crop_type == 'random':  # 先不處理 crop 大於原圖的情況
                 y0 = random.randint(0, new_height - crop_h)
                 x0 = random.randint(0, new_width - crop_w)
                 y1 = y0 + crop_h
                 x1 = x0 + crop_w
                 crop = img[y0:y1, x0:x1, :]
             else:
-                crop = cv2.resize(img, (crop_w, crop_h))  # center crop 尚未完成
-            if preprocess == "caffe":
+                crop = cv2.resize(img, (crop_w, crop_h))
+            if preprocess == 'caffe':
                 if color_mode == 'BGR':
                     crop = crop - np.array([[[103.939, 116.779, 123.68]]], dtype=np.float32)
                 elif color_mode == 'RGB':
@@ -153,33 +180,49 @@ def crop_generator(target_directory, batch_size=1, crop_type=None, crop_w=256, c
             batch_feature = np.concatenate((batch_feature, crop), 0)
             batch_label = np.concatenate((batch_label, label), 0)
         yield batch_feature, batch_label
+        file_remain_num = file_remain_num - batch_size
+        if file_remain_num<1:
+            file_remain_num = file_num
 
 
 train_data_gen = crop_generator(
-    train_dir,
-    batch_size=batch_size,
-    crop_type=crop_type,
-    crop_w=IMG_SHAPE,
-    crop_h=IMG_SHAPE,
-    resize_short_edge_max=480,
-    resize_short_edge_min=256,
-    shuffle=True,
-    color_mode="BGR",
-    seed=486
+    target_directory=train_dir,
+    final_batch_opt=train_final_batch_opt,
+    crop_type=train_crop_type,
+    crop_h=train_crop_h,
+    crop_w=train_crop_w,
+    resize_short_edge_max=train_resize_short_edge_max,
+    resize_short_edge_min=train_resize_short_edge_min,
+    horizontal_flip=train_horizontal_flip,
+    shuffle=train_shuffle,
+    shuffle_every_epoch=train_shuffle_every_epoch
 )
-
+next(train_data_gen)
 val_data_gen = crop_generator(
-    val_dir,
-    batch_size=batch_size,
-    crop_type=crop_type,
-    crop_w=IMG_SHAPE,
-    crop_h=IMG_SHAPE,
-    resize_short_edge_max=480,
-    resize_short_edge_min=256,
-    shuffle=False,
-    color_mode="BGR",
-    seed=486
+    target_directory=val_dir,
+    final_batch_opt=val_final_batch_opt,
+    crop_type=train_crop_type,
+    crop_w=train_crop_h,
+    crop_h=train_crop_w,
+    resize_short_edge_max=train_resize_short_edge_max,
+    resize_short_edge_min=train_resize_short_edge_min,
+    horizontal_flip=train_horizontal_flip,
+    shuffle=val_shuffle,
+    shuffle_every_epoch=val_shuffle_every_epoch
 )
+next(val_data_gen)
+
+# test final_batch_opt
+a = next(train_data_gen)
+file_remain_num = train_cardinality-batch_size
+batch_data_num = min(batch_size, file_remain_num)
+count = 0
+while file_remain_num > 0:
+    count = count + 1
+    print(count)
+    b = next(train_data_gen)
+    file_remain_num = file_remain_num - batch_size
+c = next(train_data_gen)
 
 # Fine tune or Retrain ResNet101
 if multi_GPU:
