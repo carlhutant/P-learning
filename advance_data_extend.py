dadcc548fb7aa82dd0e195d4f0d5629b7d9cd7d5
carlhutant <carlhutant@gmail.com>
2021-10-04 15:34:31 +0800

    2021.10.04 15:33 advance data extend npy file format included

--- advance_data_extend.py
+++ advance_data_extend.py
@@ -14,8 +14,8 @@ dataset = 'AWA2'
 # datatype: img, tfrecord
 datatype = 'img'
 # data data_advance: color_diff_121, color_diff_121_3ch, none
-data_advance = 'color_diff_121_abs_3ch'
-
+data_advance = 'color_diff_121'
+# data usage: train, val, test
 data_usage = 'train'
 
 if dataset == 'AWA2':
@@ -27,39 +27,37 @@ elif dataset == 'imagenet':
 else:
     file_type = 'none'
     class_num = -1
+    raise RuntimeError
 
-process_num = 48
+process_num = 6
 split_num = 15
 dataset_dir = configure.dataset_dir
 target_directory = Path('{}/{}/{}/none/{}/'.format(dataset_dir, dataset, datatype, data_usage))
 result_directory = Path('{}/{}/{}/{}/{}/'.format(dataset_dir, dataset, datatype, data_advance, data_usage))
-verbose = False
 # result_format: none, img, tfrecord, npy
 result_format = 'npy'
-process_type = 'none'
 
 
-# def color_diff_121(array):
-#     # filter id 121
-#     horizontal_filter = np.array([[1, 0, -1],
-#                                   [2, 0, -2],
-#                                   [1, 0, -1]], dtype="int")
-#     vertical_filter = np.array([[1, 2, 1],
-#                                 [0, 0, 0],
-#                                 [-1, -2, -1]], dtype="int")
-#     feature = np.empty(shape=array.shape[:-1] + (0,))
-#     for RGB_index in range(3):
-#         horizontal_result = signal.convolve2d(array[..., RGB_index], horizontal_filter, boundary='symm',
-#                                               mode='same')
-#         vertical_result = signal.convolve2d(array[..., RGB_index], vertical_filter, boundary='symm',
-#                                             mode='same')
-#         feature = np.concatenate(
-#             (feature, horizontal_result[..., np.newaxis], vertical_result[..., np.newaxis]), axis=-1)
-#     return feature
+def color_diff_121(array):
+    horizontal_filter = np.array([[1, 0, -1],
+                                  [2, 0, -2],
+                                  [1, 0, -1]], dtype="int")
+    vertical_filter = np.array([[1, 2, 1],
+                                [0, 0, 0],
+                                [-1, -2, -1]], dtype="int")
+    feature = np.empty(shape=array.shape[:-1] + (0,))
+    for RGB_index in range(3):
+        horizontal_result = signal.convolve2d(array[..., RGB_index], horizontal_filter, boundary='symm',
+                                              mode='same')
+        vertical_result = signal.convolve2d(array[..., RGB_index], vertical_filter, boundary='symm',
+                                            mode='same')
+        feature = np.concatenate(
+            (feature, horizontal_result[..., np.newaxis], vertical_result[..., np.newaxis]), axis=-1)
+    return feature
 
 
 def np_instance_to_tf_example(np_shape, np_feature, np_label):
-    # condirm data format
+    # confirm data format
     np_shape = np_shape.reshape(-1)
     np_shape = np.array(np_shape, dtype=np.int64)
     np_feature = np_feature.reshape(-1)
@@ -72,7 +70,7 @@ def np_instance_to_tf_example(np_shape, np_feature, np_label):
     tf_feature_feature = tf.train.Feature(float_list=tf.train.FloatList(value=np_feature))
     tf_feature_label = tf.train.Feature(float_list=tf.train.FloatList(value=np_label))
 
-    # tf.train.Feature contain multiple tf.train.feature by mapping name and feature
+    # tf.train.Features contain multiple tf.train.feature by mapping name and feature
     tf_features = tf.train.Features(
         feature={
             'shape:': tf_feature_shape,
@@ -85,58 +83,56 @@ def np_instance_to_tf_example(np_shape, np_feature, np_label):
     return serialized_example
 
 
-def file_load(instance_in):
-    image = cv2.imread(instance_in['path'])
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    # image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+def file_load(instance):
+    image = cv2.imread(str(instance['path']))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # cv2.imshow("image", image)
-
+    # cv2.waitKey()
     label = np.zeros(class_num, dtype=np.float32)
-    label[instance_in['label']] = 1
-
+    label[instance['label']] = 1
     return image, label
 
 
 def feature_processing(image):
     # # custom edge
     # # filter id 121
-    # feature = color_diff_121(image)
-    feature = image[..., [0, 2, 1]]
+    feature = color_diff_121(image)
+    # feature = image[..., [0, 2, 1]]
     return feature
 
 
-def process_func(id, instance_list):
+def process_func(process_No, instance_list):
     if result_format == 'tfrecord':
         split_count = 0
         image_count = 0
         writer = tf.io.TFRecordWriter(
-            result_directory + result_tf_file + '.tfrecord-' + str(split_count * process_num + id).zfill(5))
-        for instance in instance_list:
-            image, label = file_load(instance)
-            shape = np.array(image.shape, dtype=np.int64)
-            feature = feature_processing(image)
+            result_directory + data_usage + '.tfrecord-' + str(split_count * process_num + process_No).zfill(5))
+
+    for instance in instance_list[process_No::max(process_num, 1)]:
+        print(instance['path'])
+        image, label = file_load(instance)
+        shape = np.array(image.shape, dtype=np.int64)
+        feature = feature_processing(image)
+
+        if result_format == 'tfrecord':
             serialized_example = np_instance_to_tf_example(shape, feature, label)
             writer.write(serialized_example)
             image_count = image_count + 1
-            if id == 0:
+            if process_No == 0:
                 print(str(image_count) + '/' + str(len(instance_list)))
             if image_count % split_num == 0:
                 writer.close()
                 split_count = split_count + 1
                 # print(split_count)
                 writer = tf.io.TFRecordWriter(
-                    result_directory + result_tf_file + '.tfrecord-' + str(split_count * process_num + id).zfill(5))
+                    result_directory + data_usage + '.tfrecord-' + str(split_count * process_num + process_No).zfill(5))
+        elif result_format == 'npy':
+            np.save(Path(result_directory).joinpath(Path(instance['path']).parent.stem).joinpath(Path(instance['path']).stem), feature)
+        elif result_format == 'img':
+            a = 0
+    if result_format == 'tfrecord':
         writer.close()
-    elif result_format == 'img':
-        for instance in instance_list:
-            image, _ = file_load(instance)
-            feature = feature_processing(image)
-            split_file_name = os.path.splitext(instance['path'])
-            save_file_name = split_file_name[0] + '_' + process_type + split_file_name[1]
-            cv2.imwrite(save_file_name, feature)
-    elif result_format == 'npy':
-        for instance in instance_list:
-            a=0
+    return
 
 
 if __name__ == "__main__":
@@ -168,12 +164,11 @@ if __name__ == "__main__":
                 Path.mkdir(Path(result_directory).joinpath(d))
 
     if process_num == 0:
-        process_func(0, instance_list)
-        a = 0
+        process_func(0)
     else:
         processes = []
         for i in range(process_num):
-            processes.append(multiprocessing.Process(target=process_func, args=(i, instance_list[i::process_num])))
+            processes.append(multiprocessing.Process(target=process_func, args=(i, instance_list)))
             processes[i].start()
         for i in range(process_num):
             processes[i].join()
