cfdb0214d3eb4bf0e6c367323bed8b170f0e2352
carlhutant <carlhutant@gmail.com>
2022-03-15 20:22:31 +0800

    2022.03.15 20:21 building color swap map function without label

--- Resnet_tf_dataset.py
+++ Resnet_tf_dataset.py
@@ -58,7 +58,7 @@ def example_parse_decode(raw_example):
     return feature, label
 
 
-def random_crop(img, label, config):
+def random_crop(img, config):
     shape = tf.shape(img)
     shape = tf.slice(shape, [0], [2])
     shape_min = tf.reduce_min(shape)
@@ -69,10 +69,10 @@ def random_crop(img, label, config):
     shape = tf.cast(shape, dtype=tf.int32)
     img = tf.image.resize(images=img, size=shape, preserve_aspect_ratio=True)
     crop = tf.image.random_crop(value=img, size=(config['crop_h'], config['crop_w'], config['channel']))
-    return crop, label
+    return crop
 
 
-def random_plus2_crop(img, label, config):
+def random_plus2_crop(img, config):
     shape = tf.shape(img)
     shape = tf.slice(shape, [0], [2])
     shape_min = tf.reduce_min(shape)
@@ -83,7 +83,7 @@ def random_plus2_crop(img, label, config):
     shape = tf.cast(shape, dtype=tf.int32)
     img = tf.image.resize(images=img, size=shape, preserve_aspect_ratio=True)
     crop = tf.image.random_crop(value=img, size=(config['crop_h'] + 2, config['crop_w'] + 2, config['channel']))
-    return crop, label
+    return crop
 
 
 def color_diff_121_abs_3ch(img):
@@ -110,29 +110,44 @@ def color_diff_121_abs_3ch(img):
     return diff
 
 
-def resnet_caffe_preprocessing_rbg(feature, label):
+def resnet_caffe_preprocessing_rbg(feature):
     a = np.array([123.68, 116.779, 103.939])
     a = a[[0, 2, 1]]
     imagenet_mean = tf.constant(a, dtype=tf.float32)
     feature = tf.subtract(feature, imagenet_mean)
-    return feature, label
+    return feature
 
 
-def diy_normalizing(feature, label):
-    if dataset == 'imagenet':
-        if data_advance == 'none':
+def diy_normalizing(feature, config):
+    if config == 'default':
+        if dataset == 'imagenet':
+            if data_advance == 'none':
+                mean = np.array([122.6774804, 116.65180841, 103.99943435])
+                std = np.array([59.53750016, 58.50399986, 58.71025873])
+            else:
+                raise RuntimeError
+        elif dataset == 'AWA2':
+            if data_advance == 'none':
+                mean = np.array([119.09687091, 119.04670833, 100.78114277])
+                std = np.array([55.85246336, 54.56388751, 54.51621827])
+            elif data_advance == 'color_diff_121_abs_3ch':
+                mean = np.array([11.03982951, 10.86760973, 10.90499236])
+                std = np.array([11.97321782, 11.92262023, 11.88327279])
+            else:
+                raise RuntimeError
+        else:
+            raise RuntimeError
+    elif dataset == 'imagenet':
+        if config == 'none':
             mean = np.array([122.6774804, 116.65180841, 103.99943435])
             std = np.array([59.53750016, 58.50399986, 58.71025873])
         else:
             raise RuntimeError
     elif dataset == 'AWA2':
-        if data_advance == 'none':
+        if config == 'none':
             mean = np.array([119.09687091, 119.04670833, 100.78114277])
             std = np.array([55.85246336, 54.56388751, 54.51621827])
-        elif data_advance == 'color_sw_RBG':
-            mean = np.array([119.09687091, 100.78114277, 119.04670833])
-            std = np.array([55.85246336, 54.51621827, 54.56388751])
-        elif data_advance == 'color_diff_121_abs_3ch':
+        elif config == 'color_diff_121_abs_3ch':
             mean = np.array([11.03982951, 10.86760973, 10.90499236])
             std = np.array([11.97321782, 11.92262023, 11.88327279])
         else:
@@ -143,69 +158,57 @@ def diy_normalizing(feature, label):
     awa2_std = tf.constant(std, dtype=tf.float32)
     feature = tf.subtract(feature, awa2_mean)
     feature = tf.divide(feature, awa2_std)
-    return feature, label
-
-
-def diy_normalizing_multi_domain(feature):
-    if data_advance == 'none':
-        mean1 = np.array([119.09687091, 119.04670833, 100.78114277])
-        std1 = np.array([55.85246336, 54.56388751, 54.51621827])
-    elif data_advance == 'color_diff_121_abs_3ch':
-        mean1 = np.array([11.03982951, 10.86760973, 10.90499236])
-        std1 = np.array([11.97321782, 11.92262023, 11.88327279])
-    elif data_advance == 'color_sw_RBG':
-        mean1 = np.array([119.09687091, 119.04670833, 100.78114277])
-        std1 = np.array([55.85246336, 54.56388751, 54.51621827])
-    else:
-        raise RuntimeError
-    if data_advance2 == 'none':
-        mean2 = np.array([119.09687091, 119.04670833, 100.78114277])
-        std2 = np.array([55.85246336, 54.56388751, 54.51621827])
-    elif data_advance2 == 'color_diff_121_abs_3ch':
-        mean2 = np.array([11.03982951, 10.86760973, 10.90499236])
-        std2 = np.array([11.97321782, 11.92262023, 11.88327279])
-    else:
-        raise RuntimeError
-    mean = np.concatenate((mean1, mean2), axis=0)
-    std = np.concatenate((std1, std2), axis=0)
-    awa2_mean = tf.constant(mean, dtype=tf.float32)
-    awa2_std = tf.constant(std, dtype=tf.float32)
-    feature = tf.subtract(feature, awa2_mean)
-    feature = tf.divide(feature, awa2_std)
     return feature
 
 
-def resnet_caffe_preprocessing(feature, label):
+def resnet_caffe_preprocessing(feature):
     a = np.array([123.68, 116.779, 103.939])
     if data_advance == 'color_sw_RBG':
         a = a[[0, 2, 1]]
     imagenet_mean = tf.constant(a, dtype=tf.float32)
     feature = tf.subtract(feature, imagenet_mean)
-    return feature, label
+    return feature
 
 
-def resnet_tf_preprocessing(feature, label):
+def resnet_tf_preprocessing(feature):
     feature = tf.divide(feature, tf.constant(127.5))
     feature = tf.subtract(feature, tf.constant(1.0))
-    return feature, label
+    return feature
 
 
-def resnet_0_1_preprocessing(feature, label):
+def resnet_0_1_preprocessing(feature):
     feature = tf.divide(feature, tf.constant(255, dtype=tf.float32))
-    return feature, label
+    return feature
 
 
-def tmp_combine_6ch(feature, label, config):
-    # if data_advance != 'none' or data_advance2 != 'color_diff_121_abs_3ch':
-    #     raise RuntimeError
+def combine_6ch_and_normalizing(feature, config):
     diff = color_diff_121_abs_3ch(feature)
     diff = tf.slice(diff, [1, 1, 0], [config['crop_h'], config['crop_w'], config['channel']])
     feature = tf.slice(feature, [1, 1, 0], [config['crop_h'], config['crop_w'], config['channel']])
-    # feature = np.zeros((config['crop_h'], config['crop_w'], config['channel']), dtype=np.double)
-    # feature = tf.constant(feature, tf.float32)
+
+    # feature = diy_normalizing(feature, 'none')
+    # diff = diy_normalizing(diff, 'color_diff_121_abs_3ch')
+
     feature = tf.concat([feature, diff], 2)
-    feature = diy_normalizing_multi_domain(feature)
-    return feature, label
+    return feature
+
+
+def channel_swap(feature, config):
+    red = feature[..., 0]
+    green = feature[..., 1]
+    blue = feature[..., 2]
+    red = red[..., tf.newaxis]
+    green = green[..., tf.newaxis]
+    blue = blue[..., tf.newaxis]
+    full = tf.constant(np.zeros((config['crop_h'], config['crop_w'], 1), dtype=float), dtype=float)
+    if data_advance == 'none':
+        feature = tf.concat([red, green, blue], 2)
+    elif data_advance == 'color_sw_GBR':
+        feature = tf.concat([green, blue, red], 2)
+    elif data_advance == 'color_sw_RBG':
+        feature = tf.concat([red, blue, green], 2)
+    # return feature[..., ::1]
+    return feature
 
 
 # physical_devices = tf.config.list_physical_devices('GPU')
@@ -256,15 +259,19 @@ train_dataset.apply(tf.data.experimental.assert_cardinality(train_cardinality))
 val_dataset.apply(tf.data.experimental.assert_cardinality(val_cardinality))
 train_dataset = train_dataset.map(example_parse_decode)
 val_dataset = val_dataset.map(example_parse_decode)
-train_dataset = train_dataset.map(lambda img, label: random_crop(img, label, train_config))
-val_dataset = val_dataset.map(lambda img, label: random_crop(img, label, val_config))
-train_dataset = train_dataset.map(diy_normalizing)
-val_dataset = val_dataset.map(diy_normalizing)
+# train_dataset = train_dataset.map(lambda img, label: (random_crop(img, train_config), label))
+# val_dataset = val_dataset.map(lambda img, label: (random_crop(img, val_config), label))
+# train_dataset = train_dataset.map(lambda img, label: (channel_swap(img, train_config), label))
+# val_dataset = val_dataset.map(lambda img, label: (channel_swap(img, val_config), label))
+# train_dataset = train_dataset.map(diy_normalizing)
+# val_dataset = val_dataset.map(diy_normalizing)
 
-# train_dataset = train_dataset.map(lambda img, label: random_plus2_crop(img, label, train_config))
-# val_dataset = val_dataset.map(lambda img, label: random_plus2_crop(img, label, val_config))
-# train_dataset = train_dataset.map(lambda img, label: tmp_combine_6ch(img, label, train_config))
-# val_dataset = val_dataset.map(lambda img, label: tmp_combine_6ch(img, label, val_config))
+train_dataset = train_dataset.map(lambda img, label: (random_plus2_crop(img, train_config), label))
+val_dataset = val_dataset.map(lambda img, label: (random_plus2_crop(img, val_config), label))
+train_dataset = train_dataset.map(lambda img, label: (channel_swap(img, train_config), label))
+val_dataset = val_dataset.map(lambda img, label: (channel_swap(img, val_config), label))
+train_dataset = train_dataset.map(lambda img, label: (combine_6ch_and_normalizing(img, train_config), label))
+val_dataset = val_dataset.map(lambda img, label: (combine_6ch_and_normalizing(img, val_config), label))
 
 train_dataset = train_dataset.shuffle(buffer_size=1000, reshuffle_each_iteration=True)
 val_dataset = val_dataset.shuffle(buffer_size=1000, reshuffle_each_iteration=True)
@@ -292,8 +299,8 @@ val_dataset = val_dataset.repeat()
 # # Fine tune or Retrain ResNet101
 # import resnet
 # base_model = ResNet101(weights='imagenet', include_top=True)
-# model = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/none_color_diff_121_abs_3ch/random_crop/no_dropout/lr1e-1-ckpt-epoch0050_loss-15.3944_accuracy-0.9452_val_loss-19.5259_val_accuracy-0.7952")
-model = load_model(ckpt_dir + "lr1e-2/lr1e-2-ckpt-epoch0016_loss-0.7403_accuracy-0.8164_val_loss-1.9177_val_accuracy-0.6218")
+model = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/none_color_diff_121_abs_3ch/random_crop/no_dropout/lr1e-1-ckpt-epoch0050_loss-15.3944_accuracy-0.9452_val_loss-19.5259_val_accuracy-0.7952")
+# model = load_model(ckpt_dir + "lr1e-2/lr1e-2-ckpt-epoch0016_loss-0.7403_accuracy-0.8164_val_loss-1.9177_val_accuracy-0.6218")
 # model = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/none/random_crop/lr1e-1-ckpt-epoch0015_loss-1.6007_accuracy-0.6290_val_loss-2.7365_val_accuracy-0.5600")
 # model1 = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/none/random_crop/lr1e-1-ckpt-epoch0200_loss-0.1270_accuracy-0.9619_val_loss-5.9098_val_accuracy-0.7719")
 # model2 = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/color_diff_121_abs_3ch/random_crop/lr1e-1-ckpt-epoch0200_loss-0.1668_accuracy-0.9496_val_loss-2.7433_val_accuracy-0.7142")
@@ -330,7 +337,7 @@ model = load_model(ckpt_dir + "lr1e-2/lr1e-2-ckpt-epoch0016_loss-0.7403_accuracy
 # for layer in model.layers[-1:]:
 #     layer.trainable = True
 
-model.compile(optimizer=SGD(learning_rate=0.001, momentum=0.5, nesterov=False),
+model.compile(optimizer=SGD(learning_rate=0.1, momentum=0.9, nesterov=False),
               loss='categorical_crossentropy', metrics=['accuracy'])
 
 STEP_SIZE_TRAIN = math.ceil(train_cardinality // train_batch_size)
@@ -339,7 +346,7 @@ STEP_SIZE_VALID = math.ceil(val_cardinality // val_batch_size)
 # STEP_SIZE_TRAIN = 20
 # STEP_SIZE_VALID = 20
 
-model_checkpoint = ModelCheckpoint(ckpt_dir + 'lr1e-3-ckpt-epoch{epoch:04d}'
+model_checkpoint = ModelCheckpoint(ckpt_dir + 'lr1e-1-ckpt-epoch{epoch:04d}'
                                    + '_loss-{loss:.4f}'
                                    + '_accuracy-{accuracy:.4f}'
                                    + '_val_loss-{val_loss:.4f}'
