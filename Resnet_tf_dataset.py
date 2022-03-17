c8b131a91ce6ea6a3d9e994f6bc900b8636437b7
carlhutant <carlhutant@gmail.com>
2022-03-17 22:53:46 +0800

    2022.03.15 21:38 optimizer use Adam

--- Resnet_tf_dataset.py
+++ Resnet_tf_dataset.py
@@ -299,7 +299,7 @@ val_dataset = val_dataset.repeat()
 # import resnet
 # base_model = ResNet101(weights='imagenet', include_top=True)
 # model = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/none_color_diff_121_abs_3ch/random_crop/no_dropout/lr1e-1-ckpt-epoch0050_loss-15.3944_accuracy-0.9452_val_loss-19.5259_val_accuracy-0.7952")
-# model = load_model(ckpt_dir + "lr1e-2/lr1e-2-ckpt-epoch0016_loss-0.7403_accuracy-0.8164_val_loss-1.9177_val_accuracy-0.6218")
+model = load_model(ckpt_dir + "lr no decay/50/lr1e-3/ckpt-epoch0001_loss-0.6483_accuracy-0.8302_val_loss-0.7772_val_accuracy-0.7914")
 # model = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/none/random_crop/lr1e-1-ckpt-epoch0015_loss-1.6007_accuracy-0.6290_val_loss-2.7365_val_accuracy-0.5600")
 # model1 = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/none/random_crop/lr1e-1-ckpt-epoch0200_loss-0.1270_accuracy-0.9619_val_loss-5.9098_val_accuracy-0.7719")
 # model2 = load_model("/media/uscc/HDD1/carl/Model/AWA2/tfrecord/color_diff_121_abs_3ch/random_crop/lr1e-1-ckpt-epoch0200_loss-0.1668_accuracy-0.9496_val_loss-2.7433_val_accuracy-0.7142")
@@ -315,7 +315,7 @@ val_dataset = val_dataset.repeat()
 # tf.keras.utils.plot_model(model, to_file='model.png', show_shapes=True, show_dtype=True, show_layer_names=True,
 #                           rankdir="TB", expand_nested=False, dpi=96, )  # 儲存模型圖
 
-model = ResnetDIY.resnet50(class_num=class_num, channel=channel)
+# model = ResnetDIY.resnet50(class_num=class_num, channel=channel)
 # model = ResnetDIY.resnet101(class_num=class_num, channel=6)
 # base_model = tensorflow.keras.models.load_model(ckpt_dir + 'lr1e-4/lr1e-4-ckpt-epoch0059_loss-0.0603_accuracy-0.9831_val_loss-4.1679_val_accuracy-0.7912')
 # model = Model(inputs=model.input, outputs=model.layers[-3].output)
@@ -336,8 +336,8 @@ model = ResnetDIY.resnet50(class_num=class_num, channel=channel)
 # for layer in model.layers[-1:]:
 #     layer.trainable = True
 
-model.compile(optimizer=tfa.optimizers.SGDW(learning_rate=0.1, weight_decay=0.0001, momentum=0.9, nesterov=False),
-              loss='categorical_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer=tfa.optimizers.SGDW(learning_rate=0.00001, weight_decay=0.0001, momentum=0.9, nesterov=False), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 STEP_SIZE_TRAIN = math.ceil(train_cardinality // train_batch_size)
 STEP_SIZE_VALID = math.ceil(val_cardinality // val_batch_size)
@@ -367,7 +367,9 @@ model.fit(train_dataset,
           steps_per_epoch=STEP_SIZE_TRAIN,
           validation_data=val_dataset,
           validation_steps=STEP_SIZE_VALID,
-          callbacks=[model_checkpoint, LR]
+          callbacks=[model_checkpoint,
+                     # LR
+          ]
           )
 # model.evaluate(val_dataset, batch_size=val_batch_size)
 # b = model.layers[-1].weights[0][0]
